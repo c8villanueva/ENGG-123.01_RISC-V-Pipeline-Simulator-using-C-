70348f1138e562b83e1cb185eb8f6bce45e3c558
c8villanueva <caitlin.villanueva@gmail.com>
2025-11-13 21:32:27 +0800

    tried smth

--- temp.cpp
+++ temp.cpp
@@ -196,12 +196,12 @@ void showCode(string &address, int N, uint8_t * &instruction_memory,
   }
 }
 
-// // TBD: MOVED TO INSTRUCTION_DECODE & _EXECUTE
-// int execInstruction(unsigned int instruction, long long * &reg, 
-//                      uint8_t * &mem)
-// {
-//   // check p2_code_dump.txt
-// }
+// TBD: MOVED TO INSTRUCTION_DECODE & _EXECUTE
+int execInstruction(unsigned int instruction, long long * &reg, 
+                     uint8_t * &mem)
+{
+  // check p2_code_dump.txt
+}
 
 // PIPELINING STAGES (info from geeksforgeeks)
 
@@ -224,17 +224,16 @@ unsigned int instruction_fetch(int pc, uint8_t * &instruction_memory)
 
 // decodes instruction and register file is accessed to obtain values of registers used in instruction
 // TBD: TO FIX
-void instruction_decode(unsigned int instruction, long long *&reg,
-                        string &inst, long long &a, long long &b,
-                        long long &c, int &rd)
+void instruction_decode(unsigned int instruction, long long * &reg, 
+                        string inst, long long a, long long b, long long c)
 {
-  unsigned int opcode = instruction & 0x7F;
-  rd = (instruction >> 7) & 0x1F;
+  unsigned int opcode =  instruction & 0x7F;
+  unsigned int rd     = (instruction >> 7)  & 0x1F;
 
   // R Format
   unsigned int funct3 = (instruction >> 12) & 0x07;
-  unsigned int rs1 = (instruction >> 15) & 0x1F;
-  unsigned int rs2 = (instruction >> 20) & 0x1F;
+  unsigned int rs1    = (instruction >> 15) & 0x1F;
+  unsigned int rs2    = (instruction >> 20) & 0x1F;
   unsigned int funct7 = (instruction >> 25) & 0x7F;
 
   // I Format
@@ -242,159 +241,157 @@ void instruction_decode(unsigned int instruction, long long *&reg,
   if (immediate & 0x800) immediate |= 0xFFFFF000;
 
   // S Format
-  int imm_s = ((instruction >> 7) & 0x1F) |
+  int imm_s = ((instruction >> 7) & 0x1F) | 
               (((instruction >> 25) & 0x7F) << 5);
   if (imm_s & 0x800) imm_s |= 0xFFFFF000;
 
   // B Format
-  int imm_b = (((instruction >> 8) & 0x0F) << 1) |
-              (((instruction >> 25) & 0x3F) << 5) |
-              (((instruction >> 7) & 0x01) << 11) |
+  int imm_b = (((instruction >> 8) & 0x0F)  << 1)  | 
+              (((instruction >> 25) & 0x3F) << 5)  | 
+              (((instruction >> 7) & 0x01)  << 11) | 
               (((instruction >> 31) & 0x01) << 12);
   if (imm_b & 0x1000) imm_b |= 0xFFFFE000;
 
-  switch (opcode)
-  {
-  case 0b0110011: // R-type ADD/SUB
-    if (funct3 == 0 && funct7 == 0x00)
-    {
-      if (rd == 0)
-      {
-        cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
-      }
-      else
-      {
-        inst = "ADD";
-        a = reg[rs1];
-        b = reg[rs2];
-        c = reg[rd];
-      }
-    }
-    else if (funct3 == 0 && funct7 == 0x20)
-    {
-      if (rd == 0)
-      {
-        cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
-      } 
-      else
-      {
-        inst = "SUB";
-        a = reg[rs1];
-        b = reg[rs2];
-        c = reg[rd];
-      }
-    }
-    break;
-
-  case 0b0010011: // I-type
-    if (funct3 == 0)
-    {
-      // ADDI
-      if (rd == 0)
-      {
-        // NOP detection (addi x0, x0, 0)
-        if (!(opcode == 0b0010011 && funct3 == 0 && rs1 == 0 && immediate == 0))
+  switch (opcode) {
+    case 0b0110011: // R-type ADD/SUB
+      if (funct3 == 0 && funct7 == 0x00) 
+        if (rd == 0) 
+        {
           cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
-        else
-          cout << "NOP (addi x0, x0, 0)" << endl;
+        } 
+        else 
+        {
+          inst = "ADD";
+          a = reg[rs1];
+          b = reg[rs2];
+          c = reg[rd];
+        }
       }
-      else
-      {
-        inst = "ADDI";
-        a = reg[rs1];
-        b = immediate;
-        c = reg[rd];
+      if (funct3 == 0 && funct7 == 0x20) 
+        if (rd == 0) 
+        {
+          cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
+        } 
+        else 
+        {
+          inst = "SUB";
+          a = reg[rs1];
+          b = reg[rs2];
+          c = reg[rd];
+        }
       }
-    }
-    else if (funct3 == 1 && funct7 == 0x00)
-    {
-      // SLLI
-      unsigned int shamt = rs2;
-      if (rd == 0)
-        cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
-      else
-      {
-        inst = "SLLI";
-        a = reg[rs1];
-        b = shamt;
-        c = reg[rd];
-      }
-    }
-    break;
+      break;
 
-  // ---------------- B-type: BLT, BEQ ----------------
-  case 0b1100011:
-    if (funct3 == 0x1)
-    {
-      inst = "BLT";
-      a = reg[rs1];
-      b = reg[rs2];
-      c = imm_b;
-    }
-    else if (funct3 == 0x0)
-    {
-      inst = "BEQ";
-      a = reg[rs1];
-      b = reg[rs2];
-      c = imm_b;
-    }
-    break;
+    case 0b0010011: // I-type
+      if (funct3 == 0) 
+        if (rd == 0) 
+        {
+          if (!(opcode == 0b0010011 
+              && funct3 == 0 
+              && rs1 == 0 
+              && immediate == 0)) 
+          {
+            cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
+          } 
+          else 
+          {
+            cout << "NOP (addi x0, x0, 0)" << endl;
+          }
 
-  default:
-    inst = "UNKNOWN";
-    break;
+        } 
+        else 
+        {
+          inst = "ADDI";
+          a = reg[rs1];
+          b = immediate;
+          c = reg[rd];
+        }
+      }
+      if (funct3 == 1 && funct7 == 0)
+      { // SLLI
+        unsigned int shamt = rs2;
+        if (rd == 0) 
+        {
+          cout << "ERROR: Cannot write to x0 (rd = 0)." << endl;
+        } 
+        else 
+        {
+          inst = "SLLI";
+          a = reg[rs1];
+          b = shamt;
+          c = reg[rd];
+        }
+      }
+      break;
+
+    case 0b1100011: // B-type Branch instructions
+      if (funct3 == 0x1) 
+        { // BLT 
+          inst = "BLT";
+          a = reg[rs1];
+          b = reg[rs2];
+          c = imm_b;
+        }
+        else if (funct3 == 0x0)
+        { // BEQ
+          inst = "BEQ";
+          a = reg[rs1];
+          b = reg[rs2];
+          c = imm_b;
+        }
+      break;
+
+    default: 
+      inst = "UNKNOWN";
+      // a = 0;
+      // b = 0;
+      // c = 0;
+      // rd = 0;
   }
 }
 
 // some activities are done such as ALU operations
 // TBD: TO ADD VALIDATION IF NEEDED
-void instruction_execute(string &inst, long long &a, long long &b,
-                         long long &c, int &rd, int &pc_offset)
+void instruction_execute(string &inst, long long &a, long long &b, long long &c, int &rd)
 {
-  if (inst == "ADD")
+  if (inst == "ADD") 
   {
     c = a + b;
-    pc_offset = 4;
+    pc_offset += 4;
   }
-  else if (inst == "SUB")
+  else if (inst == "SUB") 
   {
     c = a - b;
-    pc_offset = 4;
+    pc_offset += 4;
   }
-  else if (inst == "ADDI")
+  else if (inst == "ADDI") 
   {
     c = a + b;
-    pc_offset = 4;
+    pc_offset += 4;
   }
   else if (inst == "SLLI")
   {
     c = a << b;
-    pc_offset = 4;
+    pc_offset += 4;
   }
-  else if (inst == "BLT")
+  else if (inst == "BLT") 
   {
-    if (a < b)
-      pc_offset = c; // branch taken
-    else
-      pc_offset = 4; // next instruction
+    if (a < b) 
+    {
+      pc_offset = c;
+    } 
   }
   else if (inst == "BEQ")
   {
-    if (a == b)
+    if (a == b) 
+    {
       pc_offset = c;
-    else
-      pc_offset = 4;
-  }
-  else
-  {
-    pc_offset = 4;
+    } 
   }
 }
 
 // memory operands are read and written from/to the memory that is present in the instruction
-void memory_access(string &inst, long long &a, long long &b, 
-                   long long &c, int &rd, uint8_t *&mem, 
-                   const int memory_size)
+void memory_access(string &inst, long long &a, long long &b, long long &c, int &rd, uint8_t * &mem)
 {
   if (inst == "LD") 
   {
@@ -435,21 +432,16 @@ void write_back(string &inst, long long &c, int &rd, long long * &reg)
 }
 
 // executes RISC-v instructions in a pipelined manner
-int pipeline_loop(unsigned int instr, long long *&reg, uint8_t *&mem, const int memory_size)
+void pipeline_loop(string &inst, long long &a, long long &b, long long &c, int &rd, long long * &reg)
 {
-  string inst = "";
-  long long a = 0, b = 0, c = 0;
-  int rd = 0;
-  int pc_offset = 4;
-
-  unsigned int fetched = instr;
-  instruction_decode(fetched, reg, inst, a, b, c, rd);
-  instruction_execute(inst, a, b, c, rd, pc_offset);
-  memory_access(inst, a, b, c, rd, mem, memory_size);
-  write_back(inst, c, rd, reg);
-
-  cout << "[PIPELINE] " << inst << " executed." << endl;
-  return pc_offset;
+  while(true)
+  {
+    write_back();
+    memory_access();
+    instruction_execute();
+    instruction_decode();
+    instruction_fetch();
+  }
 }
 
 int main()
@@ -475,8 +467,7 @@ int main()
   uint8_t *data_memory = new uint8_t [memory_size]; 
   uint8_t *instruction_memory = new uint8_t [memory_size];
   
-  for(int i=0; i<memory_size; i++) 
-  {
+  for(int i=0; i<memory_size; i++) {
     data_memory[i] = 0;
     instruction_memory[i] = 0;
   }
@@ -663,8 +654,7 @@ int main()
              << setw(8) << setfill('0') << PC << dec << "...\n";
 
         // TBD: was previously exec_instruction
-        // int pcOffset = pipeline_loop(instr, registers, data_memory);
-        int pcOffset = pipeline_loop(instr, registers, data_memory, memory_size);
+        int pcOffset = pipeline_loop(instr, registers, data_memory);
 
         PC += pcOffset; 
       }
